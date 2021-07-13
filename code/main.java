dd38d8bee137ca67e322994491875cb806fda6b4
FahadAlmani <82783810+FahadAlmani@users.noreply.github.com>
2021-07-13 23:40:17 +0300

    Update main.java

--- code/main.java
+++ code/main.java
@@ -6,10 +6,10 @@ public class main {
 	public static void main(String[] args) {
 		ArrayList<Person> personList = new ArrayList<Person>();
 		Scanner input = new Scanner(System.in);
-		System.out.print("enter number of persons:");
+		System.out.print("Enter number of persons:");
 		int numberP = input.nextInt();
 		for (int i = 0; i < numberP; i++) {
-			System.out.print("number of mint's to acros the bridge:");
+			System.out.print("Number of minutes to across the bridge for person(" + (char) (i + 'a') + "):");
 			int cost = input.nextInt();
 			personList.add(new Person((char) (i + 'a'), cost));
 		}
